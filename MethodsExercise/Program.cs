bba95016037bd6dc52a8b9ccc6c99bc5b718e3a6
William Cho <williamcho89@gmail.com>
2021-01-29 07:13:15 -0500

    final edit

---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            
+            
+            
+            
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! What's your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+
+        }
+    }
+}
